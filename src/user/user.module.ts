ba574b2458b6fccd0afb22cdec1d48bb9e2cccd6
Nathália Pereira <nathalia.pereira252000@gmail.com>
2022-03-25 14:02:57 -0300

    Remove edição de usuário

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,9 +1,8 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
-import { UserService } from './user.service';
 
 @Module({
   controllers: [UserController],
-  providers: [UserService]
+  providers: []
 })
 export class UserModule {}
