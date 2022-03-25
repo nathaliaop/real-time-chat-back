4948038c948e6a167c654176af5ceb7ed9d0eb8f
Nathália Pereira <nathalia.pereira252000@gmail.com>
2022-03-25 15:53:22 -0300

    feat: Adiciona métodos get e post de mensagens

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { MessageModule } from './message/message.module';
 
 @Module({
   imports: [
@@ -12,7 +13,8 @@ import { PrismaModule } from './prisma/prisma.module';
     }),
     AuthModule,
     UserModule,
-    PrismaModule
+    PrismaModule,
+    MessageModule
   ],
 })
 export class AppModule {}
