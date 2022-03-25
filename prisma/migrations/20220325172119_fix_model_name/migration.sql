c2ef9ab311f91ea6263b33893db3014a13798f63
Nathália Pereira <nathalia.pereira252000@gmail.com>
2022-03-25 15:52:55 -0300

    feat: Adiciona model de mensagem e relaciona com a model de usuário

---
+++ prisma/migrations/20220325172119_fix_model_name/migration.sql
@@ -0,0 +1,25 @@
+/*
+  Warnings:
+
+  - You are about to drop the `bookmarks` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "bookmarks" DROP CONSTRAINT "bookmarks_userId_fkey";
+
+-- DropTable
+DROP TABLE "bookmarks";
+
+-- CreateTable
+CREATE TABLE "messages" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "text" TEXT NOT NULL,
+    "userId" INTEGER NOT NULL,
+
+    CONSTRAINT "messages_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "messages" ADD CONSTRAINT "messages_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
