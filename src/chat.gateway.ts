68c6a1c50eff32e3ab4f25e6c0220100cb3cc4d5
Nathália Pereira <nathalia.pereira252000@gmail.com>
2022-03-28 15:24:54 -0300

    feat: Cria eventos de conexão e desconexão para exibir usuários online

--- src/chat.gateway.ts
+++ src/chat.gateway.ts
@@ -1,4 +1,4 @@
-import { Logger, UseGuards } from '@nestjs/common';
+import { Logger } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import {
   OnGatewayConnection,
@@ -10,6 +10,12 @@ import {
 } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
 import { MessageService } from './message/message.service';
+import { UserService } from './user/user.service';
+
+type User = {
+  id: number,
+  username: string
+}
 
 type Payload = {
   text: string;
@@ -19,9 +25,13 @@ type Payload = {
 export class ChatGateway
   implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
 {
+
+  connectedUsers: any = [];
+
   // prisma
   constructor(
     private messageService: MessageService,
+    private userService: UserService,
     private jwtService: JwtService,
   ) {}
 
@@ -56,22 +66,49 @@ export class ChatGateway
   }
 
   handleConnection(client: Socket) {
-    /*const message = {
-      name: client.id,
-      text: 'O usuário se conectou :D',
-    };
+    let auth_token = client.handshake.headers.authorization;
 
-    this.server.emit('msgToClient', message);*/
-    this.logger.log(`Client connected: ${client.id}`);
+    const decodedJwtAccessToken = this.jwtService.decode(auth_token.split(' ')[1]);
+
+      if (decodedJwtAccessToken) {
+        const userId = decodedJwtAccessToken.sub
+
+        this.userService.getUser(
+          userId
+        )
+        .then((user: User) => {
+          const userDto = {
+            id: userId,
+            username: user.username
+          };
+          this.connectedUsers = [...this.connectedUsers, userDto];
+          this.logger.log(`Client connected: ${userDto.username}`);
+          this.server.emit('users', this.connectedUsers);
+        })
+      }
   }
 
   handleDisconnect(client: Socket) {
-    /*const message = {
-      name: client.id,
-      text: 'O usuário se desconectou :(',
-    };
+    let auth_token = client.handshake.headers.authorization;
 
-    this.server.emit('msgToClient', message);*/
-    this.logger.log(`Client disconnected: ${client.id}`);
+    const decodedJwtAccessToken = this.jwtService.decode(auth_token.split(' ')[1]);
+
+      if (decodedJwtAccessToken) {
+        const userId = decodedJwtAccessToken.sub
+
+        this.userService.getUser(
+          userId
+        )
+        .then((user: any) => {
+          const userDto = {
+            id: userId,
+            createdAt: Date.now(),
+            username: user.username
+          };
+          this.connectedUsers = this.connectedUsers.filter(connectedUser => connectedUser.id !== userId);
+          this.logger.log(`Client disconnected: ${userDto.username}`);
+          this.server.emit('users', this.connectedUsers);
+        })
+      }
   }
 }
