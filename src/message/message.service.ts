f1e448b2e5104fb9c506c8cf42ae3626ea290696
Nathália Pereira <nathalia.pereira252000@gmail.com>
2022-03-29 15:31:51 -0300

    feat: Implementa função de deletar mensagens

--- src/message/message.service.ts
+++ src/message/message.service.ts
@@ -4,56 +4,73 @@ import { CreateMessageDto } from './dto/create-message-dto';
 
 @Injectable()
 export class MessageService {
-    constructor(
-        private prisma:PrismaService,
-    ) {}
+  constructor(private prisma: PrismaService) {}
 
-    async getMessages() {
-        try {
-
-            const messages = await this.prisma.message.findMany(
-                {
-                    select: {
-                        id: true,
-                        createdAt: true,
-                        text: true,
-                        user: {
-                            select: {
-                                username: true
-                            }
-                        }
-                    },
-                    orderBy: {
-                        createdAt: 'asc'
-                    }
-                }
-            );
-            return messages;
-        }
-        catch {
-            throw new ForbiddenException('Can\'t get messages')
-        }
-    }
-
-    async createMessage(userId: number, dto: CreateMessageDto) {
-        console.log(dto)
-         //save the new message in the db
-        const message = await this.prisma.message.create({
-            data: {
-                text: dto.text,
-                userId,
-            },
+  async getMessages() {
+    try {
+      const messages = await this.prisma.message.findMany({
+        select: {
+          id: true,
+          createdAt: true,
+          text: true,
+          user: {
             select: {
-                id: true,
-                createdAt: true,
-                text: true,
-                user: {
-                    select: {
-                        username: true
-                    }
-                }
-            }
-        })
-        return message;
+							id: true,
+              username: true,
+            },
+          },
+        },
+        orderBy: {
+          createdAt: 'asc',
+        },
+      });
+      return messages;
+    } catch {
+      throw new ForbiddenException("Can't get messages");
     }
+  }
+
+  async createMessage(userId: number, dto: CreateMessageDto) {
+    console.log(dto);
+    //save the new message in the db
+    const message = await this.prisma.message.create({
+      data: {
+        text: dto.text,
+        userId,
+      },
+      select: {
+        id: true,
+        createdAt: true,
+        text: true,
+        user: {
+          select: {
+						id: true,
+            username: true,
+          },
+        },
+      },
+    });
+    return message;
+  }
+
+  async deleteMessageById(userId: number, messageId: number) {
+    // recupera a mensagem pelo id
+    const message = await this.prisma.message.findUnique({
+      where: {
+        id: messageId,
+      },
+    });
+
+    // confere se o usuário é autor da mensagem
+    if (!message || message.userId !== userId) {
+      throw new ForbiddenException('Access to resources denied');
+    }
+
+    // deleta mensagem
+		await this.prisma.message.delete({
+			where: {
+				id: messageId,
+			}
+		});
+  }
 }
